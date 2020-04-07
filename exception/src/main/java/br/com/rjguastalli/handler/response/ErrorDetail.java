4e35b21185d120c486fefcef75410eb3043996a6
Ronaldo Guastalli <guastalli31@gmail.com>
2020-04-06 23:32:07 -0300

    implementa exception (GenericException)

---
+++ exception/src/main/java/br/com/rjguastalli/handler/response/ErrorDetail.java
@@ -0,0 +1,16 @@
+package br.com.rjguastalli.handler.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorDetail {
+
+    private String type;
+    private String message;
+}
